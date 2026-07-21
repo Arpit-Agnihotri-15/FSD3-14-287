166685c312b9566c7d91f832cdbb3455882dcf05
Arpit Agnihotri <arpit151604@gmail.com>
2026-07-21 10:04:30 +0530

    10.04

--- Lab1/eventsEmitter.js
+++ Lab1/eventsEmitter.js
@@ -6,27 +6,26 @@ const sayHi = (name) =>{
     console.log(`loggedin ${name}`);
 };
 
+task.once('greet',()=>{
+    console.log("System Started...");
+})
+
 task.on('greet',sayHi)
 
 task.on('greet',()=>{
     console.log("logged Out");
 });
 
-task.once('greet',()=>{
-    console.log("System Started...");
-})
-
-task.off("exit"(name) =>{
-    console.log(`system is shutting down by $(name)`);
+task.once("exit", (name) => {
+    console.log(`system is shutting down by ${name}`);
 });
-
-
-
 task.emit('greet','Arpit Agnihotri');
-//task.emit('greet','arpit');
-//task.emit('greet','arpit agnihotri');
-//task.emit("exit",'Manager');
-
-const shuttingDown=(name)=>
-
+console.log();
+task.emit('greet','arpit');
+console.log();
+task.off('greet',sayHi);  // remove the listener sayHi
+task.emit('greet','arpit agnihotri');  //
+console.log();
+task.emit("exit",'Manager');   // execute only once
+task.emit("exit",'Employee');  //won't affect
 task.emit('greet','Manager');
