8044b67f08266412745b8a9a8c3d3774f7d1328f
Arpit Agnihotri <arpit151604@gmail.com>
2026-09-17 13:59:47 +0530

    13:59

--- Lab5/app.js
+++ Lab5/app.js
@@ -1,11 +1,32 @@
 import express from 'express'
+
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("<h1> Hello Express </h1>");
 });
-//app.use("*", (req, res) => {
-//    res.send("route not found")
-//})
 
-app.listen(4000, () => console.log("Server is running "));
+app.get('/about', (req, res) => {
+    res.end("We are Full Stack Developer");
+});
+
+app.post('/login', (req, res) => {
+    res.send({msg: 'user login'});
+});
+
+app.put('/user/update/1', (req, res) => {
+    res.send({msg: 'user update'});
+});
+
+app.delete('/users/1', (req, res) => {
+    res.send({msg: 'remove user 1'});
+});
+
+app.use((req, res) => {
+    res.status(404).send("Not Found");
+});
+
+
+
+app.listen(3333, () => console.log("Server is running "));
+
